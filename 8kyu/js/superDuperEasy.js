22b4005c8ea432e19f1e1c353d4493b60ed71258
Amelia Dutta <49182604+amelia2802@users.noreply.github.com>
2026-08-20 22:19:37 +0530

    Add superDuperEasy function with error handling
    Implement a function that multiplies input by 50 and adds 6, returning 'Error' for string inputs.

---
+++ 8kyu/js/superDuperEasy.js
@@ -0,0 +1,33 @@
+/**
+Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+*/
+
+/*--- Solution ---*/
+function problem(x){
+  //your code here
+  
+  if(typeof x === 'string'){
+    return "Error";
+  }
+  return (x * 50) + 6;
+}
+
+/*--- Test ---*/
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(problem("hello"), "Error");
+    assert.strictEqual(problem(1), 56);
+    assert.strictEqual(problem(5), 256);
+    assert.strictEqual(problem(0), 6);
+    assert.strictEqual(problem(1.2), 66);
+    assert.strictEqual(problem(3), 156);
+    assert.strictEqual(problem("RyanIsCool"), "Error");
+    assert.strictEqual(problem(-3), -144);
+    assert.strictEqual(problem(""), "Error");
+    assert.strictEqual(problem(0.03), 7.5);
+  })
+})
