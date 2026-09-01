96c87d26763b83958189d4926d287779f79cd65b
Amelia Dutta <49182604+amelia2802@users.noreply.github.com>
2026-09-01 22:58:31 +0530

    Implement function to convert name to initials
    This function takes a two-word name and returns the initials in capital letters separated by a dot. It includes tests to verify the functionality.

---
+++ 8kyu/js/abbrevName.js
@@ -0,0 +1,36 @@
+/**
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+*/
+
+/*--- Solution ---*/
+function abbrevName(name){
+
+    // code away
+  
+  let fullName = name.split(" ");
+  return (fullName[0].charAt(0)+"."+fullName[fullName.length-1].charAt(0));
+
+}
+
+/*--- Test ---*/
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(abbrevName("Sam Harris"), "S.H");
+    assert.strictEqual(abbrevName("Patrick Feenan"), "P.F");
+    assert.strictEqual(abbrevName("Evan Cole"), "E.C");
+    assert.strictEqual(abbrevName("P Favuzzi"), "P.F");
+    assert.strictEqual(abbrevName("David Mendieta"), "D.M");
+  });
+});
